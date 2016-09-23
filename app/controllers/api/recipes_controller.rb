86228c3eb06184b5f26de76abaa7df0625d2b9f5
Jesus Roman <email": null>
2016-09-23 18:20:52 -0500

    Add test post route

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,12 +1,14 @@
 module Api
   class RecipesController < ApplicationController
     respond_to :json
-    def all
+
+    def testpost
+      p params
+
       # Make sure to downcase
       # ingredients_list = params["ingredients"]
       # ingredients_list = ingredients_list.downcase
 
-      api = Spoonacular::API.new(ENV["SPOONACULAR_API_KEY"])
       # if Rails.cache.read(ingredients_list)
       #   response = Rails.cache.read(ingredients_list)
       # else
@@ -19,53 +21,62 @@ module Api
       # p 'This is what the api instance looks like:::::::'
       # response = api.find_by_ingredients('chicken tomatoes')
 
+      render json: {hello: "World"}
+    end
+
+
+    def all
+
+      api = Spoonacular::API.new(ENV["SPOONACULAR_API_KEY"])
+
+
       render json: {body: [
-{
-id: 649495,
-title: "Lemon and Garlic Slow Roasted Chicken",
-image: "https://spoonacular.com/recipeImages/Lemon-and-Garlic-Slow-Roasted-Chicken-649495.jpg",
-imageType: "jpg",
-usedIngredientCount: 1,
-missedIngredientCount: 2,
-likes: 1
-},
-{
-id: 640803,
-title: "Crispy Buttermilk Fried Chicken",
-image: "https://spoonacular.com/recipeImages/Crispy-Buttermilk-Fried-Chicken-640803.jpg",
-imageType: "jpg",
-usedIngredientCount: 1,
-missedIngredientCount: 2,
-likes: 53
-},
-{
-id: 634485,
-title: "Bbq Chicken Pizza",
-image: "https://spoonacular.com/recipeImages/Bbq-Chicken-Pizza-634485.jpg",
-imageType: "jpg",
-usedIngredientCount: 1,
-missedIngredientCount: 2,
-likes: 1
-},
-{
-id: 715525,
-title: "Slow Cooker Rosemary Whole Chicken",
-image: "https://spoonacular.com/recipeImages/slow-cooker-rosemary-whole-chicken-715525.jpg",
-imageType: "jpg",
-usedIngredientCount: 1,
-missedIngredientCount: 3,
-likes: 991
-},
-{
-id: 634463,
-title: "Batter Fried Chicken",
-image: "https://spoonacular.com/recipeImages/Batter-Fried-Chicken-634463.jpg",
-imageType: "jpg",
-usedIngredientCount: 1,
-missedIngredientCount: 3,
-likes: 8
-}
-]}
+                      {
+                      id: 649495,
+                      title: "Lemon and Garlic Slow Roasted Chicken",
+                      image: "https://spoonacular.com/recipeImages/Lemon-and-Garlic-Slow-Roasted-Chicken-649495.jpg",
+                      imageType: "jpg",
+                      usedIngredientCount: 1,
+                      missedIngredientCount: 2,
+                      likes: 1
+                      },
+                      {
+                      id: 640803,
+                      title: "Crispy Buttermilk Fried Chicken",
+                      image: "https://spoonacular.com/recipeImages/Crispy-Buttermilk-Fried-Chicken-640803.jpg",
+                      imageType: "jpg",
+                      usedIngredientCount: 1,
+                      missedIngredientCount: 2,
+                      likes: 53
+                      },
+                      {
+                      id: 634485,
+                      title: "Bbq Chicken Pizza",
+                      image: "https://spoonacular.com/recipeImages/Bbq-Chicken-Pizza-634485.jpg",
+                      imageType: "jpg",
+                      usedIngredientCount: 1,
+                      missedIngredientCount: 2,
+                      likes: 1
+                      },
+                      {
+                      id: 715525,
+                      title: "Slow Cooker Rosemary Whole Chicken",
+                      image: "https://spoonacular.com/recipeImages/slow-cooker-rosemary-whole-chicken-715525.jpg",
+                      imageType: "jpg",
+                      usedIngredientCount: 1,
+                      missedIngredientCount: 3,
+                      likes: 991
+                      },
+                      {
+                      id: 634463,
+                      title: "Batter Fried Chicken",
+                      image: "https://spoonacular.com/recipeImages/Batter-Fried-Chicken-634463.jpg",
+                      imageType: "jpg",
+                      usedIngredientCount: 1,
+                      missedIngredientCount: 3,
+                      likes: 8
+                      }
+                      ]}
     end
 
     def show
