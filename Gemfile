fbf254036d11f35e65bd3598499a88f96c4cdb15
Matthew Behrens & Jesus Roman <pair+MatthewBehrens.romanjesus@devbootcamp.com>
2016-09-23 15:29:57 -0500

    Add env files to gitignore and pseudocode the memchachier steps

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'spoonacular', '~> 0.0.2'
 
 gem 'json'
 
+gem 'dotenv-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -2,62 +2,23 @@ module Api
   class RecipesController < ApplicationController
     respond_to :json
     def all
-      p 'This is what the api instance looks like:::::::'
 
+      # Make sure to downcase
+      # ingredients_list = params["ingredients"]
+      api = Spoonacular::API.new(ENV["SPOONACULAR_API_KEY"])
+      # if Rails.cache.read(ingredients_list)
+      #   response = Rails.cache.read(ingredients_list)
+      # else
+      #   # make the API Call
+      #   # response = api.find_by_ingredients(ingredients_list)
+      #   # Save response to the cache
+      #   # Rails.cache.write(ingredients_list,response)
+      # end
+
+      # p 'This is what the api instance looks like:::::::'
       # response = api.find_by_ingredients('chicken tomatoes')
 
-      response = "{raw_body: '[{'id':649495,'title':'Lemon and Garlic Slow Roasted Chicken','image':'https://spoonacular.com/recipeImages/Lemon-and-Garlic-Slow-Roasted-Chicken-649495.jpg','imageType':'jpg','usedIngredientCount':1,'missedIngredientCount':2,'likes':1},{'id':640803,'title':'Crispy Buttermilk Fried Chicken','image':'https://spoonacular.com/recipeImages/Crispy-Buttermilk-Fried-Chicken-640803.jpg','imageType':'jpg','usedIngredientCount':1,'missedIngredientCount':2,'likes':53},{'id':634485,'title':'Bbq Chicken Pizza','image':'https://spoonacular.com/recipeImages/Bbq-Chicken-Pizza-634485.jpg','imageType':'jpg','usedIngredientCount':1,'missedIngredientCount':2,'likes':1},{'id':715525,'title':'Slow Cooker Rosemary Whole Chicken','image':'https://spoonacular.com/recipeImages/slow-cooker-rosemary-whole-chicken-715525.jpg','imageType':'jpg','usedIngredientCount':1,'missedIngredientCount':3,'likes':991},{'id':634463,'title':'Batter Fried Chicken','image':'https://spoonacular.com/recipeImages/Batter-Fried-Chicken-634463.jpg','imageType':'jpg','usedIngredientCount':1,'missedIngredientCount':3,'likes':8}]',
-body: [
-{
-id: 649495,
-title: 'Lemon and Garlic Slow Roasted Chicken',
-image: 'https://spoonacular.com/recipeImages/Lemon-and-Garlic-Slow-Roasted-Chicken-649495.jpg',
-imageType: 'jpg',
-usedIngredientCount: 1,
-missedIngredientCount: 2,
-likes: 1
-},
-{
-id: 640803,
-title: 'Crispy Buttermilk Fried Chicken',
-image: 'https://spoonacular.com/recipeImages/Crispy-Buttermilk-Fried-Chicken-640803.jpg',
-imageType: 'jpg',
-usedIngredientCount: 1,
-missedIngredientCount: 2,
-likes: 53
-},
-{
-id: 634485,
-title: 'Bbq Chicken Pizza',
-image: 'https://spoonacular.com/recipeImages/Bbq-Chicken-Pizza-634485.jpg',
-imageType: 'jpg',
-usedIngredientCount: 1,
-missedIngredientCount: 2,
-likes: 1
-},
-{
-id: 715525,
-title: 'Slow Cooker Rosemary Whole Chicken',
-image: 'https://spoonacular.com/recipeImages/slow-cooker-rosemary-whole-chicken-715525.jpg',
-imageType: 'jpg',
-usedIngredientCount: 1,
-missedIngredientCount: 3,
-likes: 991
-},
-{
-id: 634463,
-title: 'Batter Fried Chicken',
-image: 'https://spoonacular.com/recipeImages/Batter-Fried-Chicken-634463.jpg',
-imageType: 'jpg',
-usedIngredientCount: 1,
-missedIngredientCount: 3,
-likes: 8
-}
-]
-}"
-
-  obj = ActiveSupport::JSON.decode(response)
-      render json: obj
+      render json: {"hello"=> "World"}
     end
   end
 end
