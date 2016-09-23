778706f938f1a91871d94cef77909c5d7812991c
Jesus Roman <email": null>
2016-09-23 18:45:11 -0500

    Unprotecting from forgery to allow to respond to request from an outside domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ require "application_responder"
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
+  respond_to :json
 
-  protect_from_forgery with: :exception
+  protect_from_forgery
+  # with: :exception
 end
