405c8570dcbefbf0ec76b2bb6ce36b4613dde24b
Jesus Roman <email": null>
2016-09-24 16:57:11 -0500

    Add devise and omni auth gems, created user model and devise token auth initializer

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'json'
 
 gem 'dotenv-rails'
 gem 'dalli'
+gem 'omniauth'
+gem 'devise_token_auth'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 require "application_responder"
 
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
   include ApplicationHelper
   self.responder = ApplicationResponder
   respond_to :html
