78c55f5f29e34def18ca538370563c69b2ef6bf0
Matthew Behrens <mattbehrens96@gmail.com>
2016-09-26 14:37:31 -0500

    fixed bugs in the list controller

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -5,8 +5,10 @@ module Api
 
     def show
       @user = current_api_user
+      if !@user.kitchen_list
+        KitchenList.new(user: @user)
+      end
       @ingredients = @user.kitchen_list.ingredients
-
       render json: @ingredients.as_json
     end
 
